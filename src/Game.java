765c6fc67d9da22263cd19d2469766fdf501a022
Vladislav Trublovsky <embasis@gmail.com>
2015-04-19 13:32:05 +0300

    Fixed bullet fire animation

--- src/Game.java
+++ src/Game.java
@@ -54,6 +54,7 @@ public class Game {
     private ArrayList<Rocket> rocketsList;
     // List of all the rockets smoke.
     private ArrayList<RocketSmoke> rocketSmokeList;
+    // Animation when player fires a bullet
     private ArrayList<Animation> bulletFire;
     // Image for the sky color.
     private BufferedImage skyColorImg;
@@ -123,8 +124,8 @@ public class Game {
         explosionsList = new ArrayList<Animation>();
         
         bulletsList = new ArrayList<Bullet>();
-        bulletFire = new ArrayList<Animation>();
         rocketsList = new ArrayList<Rocket>();
+        bulletFire = new ArrayList<Animation>();
         rocketSmokeList = new ArrayList<RocketSmoke>();
         
         // Moving images.
@@ -205,8 +206,6 @@ public class Game {
         cloudLayer2Moving.Initialize(cloudLayer2Img, -2, 0);
         mountainsMoving.Initialize(mountainsImg, -1, Framework.frameHeight - groundImg.getHeight() - mountainsImg.getHeight() + 40);
         groundMoving.Initialize(groundImg, -1.2, Framework.frameHeight - groundImg.getHeight());
-       
-
     }
      
     
@@ -318,14 +317,19 @@ public class Game {
            
         }
         
+        // Draws bullet fire animation
         for(int i = 0; i < bulletFire.size(); i++)
         {
-        	if(bulletFire.get(i).active){
-        		bulletFire.get(i).Draw(g2d);
+        	Animation an = bulletFire.get(i);
+        	if(an.active) {
+        		an.changeCoordinates(player.xCoordinate + player.helicopterBodyImg.getWidth() - 61,
+        							 	 player.yCoordinate + player.helicopterBodyImg.getHeight() - 40);
+        		an.Draw(g2d);
         	} else {
-        	 bulletFire.remove(i);
+        		bulletFire.remove(i--);
         	}
         }
+        
         // Draws all the rockets. 
         for(int i = 0; i < rocketsList.size(); i++)
         {
@@ -485,10 +489,8 @@ public class Game {
             if(!fireGun.isActive()){
             	fireGun.setFramePosition(0);
             }
+            bulletFire.add(new Animation(shotImg, 40, 50, 5, 40, false, 0, 0, 0));
             bulletsList.add(b);
-            
-            Animation shotAnim = new Animation(shotImg, 40, 50, 5, 20, false, player.xCoordinate + player.helicopterBodyImg.getWidth() - 61, player.yCoordinate + player.helicopterBodyImg.getHeight() - 40, 0);
-            bulletFire.add(shotAnim);
         }
     }
     
